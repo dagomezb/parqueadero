9ae1e88e0d1b994464540d3cb4bf1fab0bf1b68c
Duván Alberto Gomez Betancur <dgomez@UCO.EDU.CO>
2018-10-22 17:28:46 -0500

    Nueva clase ParqueaderoMoto

---
+++ Parqueadero/src/com/uco/parqueadero/ParqueaderoMoto.java
@@ -0,0 +1,14 @@
+package com.uco.parqueadero;
+
+public class ParqueaderoMoto extends Parqueadero {
+
+	public ParqueaderoMoto(double tarifa, double dineroCaja, int cantidadParqueaderos) {
+		super(tarifa, dineroCaja, cantidadParqueaderos);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void IngresarMoto() {
+		
+	}
+
+}
